0649a09b131e97313188afac423011a336dc5820
simplyadil <95224704+simplyadil@users.noreply.github.com>
2022-04-15 15:40:07 +0000

    commit 2

---
+++ gestion_notes/src/gestion_notes/ListEtudiants.java
@@ -0,0 +1,21 @@
+package gestion_notes;
+
+import java.util.ArrayList;
+
+public class ListEtudiants extends ArrayList<etudiant> {
+	
+   /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+//redefinition de add
+public boolean add(etudiant e) {
+	   if(this.indexOf(e)>0) {
+		   int i=indexOf(e);
+		   super.set(i, e);
+		   return false;
+	   }
+	return super.add(e);
+   }
+}
+//redefinition
--- gestion_notes/src/gestion_notes/etudiant.java
+++ gestion_notes/src/gestion_notes/etudiant.java
@@ -1,7 +1,28 @@
 package gestion_notes;
 
-public class etudiant {
+public class etudiant implements  Comparable<etudiant>{
      private int id;
-     private string nom:
+     private String nom;
      private double note;
+     
+     etudiant ( int I, String N){
+    	 this.note=10;
+    	 this.id=I;
+    	 this.nom=N;
+     }
+public String toString() {
+	return this.nom +" : "+this.note;
+  }
+
+
+public boolean equals(etudiant obj) {
+	if(this.id==obj.id) {
+		return true;
+	}else {
+		return false;
+	}
+  }
+public int compareTo(etudiant e) {
+	if (this.note>=e.note) return 1;
+	if (this.note==e.note) return 0;
 }
